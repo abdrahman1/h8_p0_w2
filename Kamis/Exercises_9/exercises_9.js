f283a0770f2db94c343648964566594176bef181
abdrahmanw <abrahmanw@gmail.com>
2018-10-25 20:47:30 +0700

    tambah dua file di folder kamis dan edit file script.js di folder Selasa

---
+++ Kamis/Exercises_9/exercises_9.js
@@ -0,0 +1,33 @@
+/**
+ * H8_p0_w2_E8
+ * Mengenal Penggunaan Function
+ * ========
+ */
+
+// ========[1]
+function shoutOut() {
+    return 'Halo Function!';
+}
+console.log(shoutOut());
+
+// ========[2]
+function calculateMultiply(num1, num2) {
+    var Multiply = num1 * num2;
+    return Multiply;
+}
+var hasilPerkalian = calculateMultiply(2, 5);
+console.log(hasilPerkalian); 
+
+// ========[2]
+
+function processSentence(nama, umur, alamat, hobby) {
+    return 'Nama saya '+nama+', umur saya '+umur+' tahun, alamat saya di '+alamat+', dan saya punya hobby yaitu '+hobby
+}
+
+var name = 'Agus';
+var age = 30;
+var address = 'Jln. Malioboro, Yogjakarta';
+var hobby = 'Gaming';
+
+var fullSentence = processSentence(name, age, address, hobby);
+console.log(fullSentence);
--- Selasa/Exercises_3/exercises3/script.backup.js
+++ Selasa/Exercises_3/exercises3/script.backup.js
@@ -3,47 +3,72 @@
  * jalankan di repl atau browser
  */
 
-console.log('\t\tSelamat datang di game dystopi \n')
+console.log('\t\tProxytia \n')
 var promptPlay = false;
 var peran1 = 'Ksatria';
 var peran2 = 'Tabib';
 var peran3 = 'Penyihir';
+var senjata;
 while (promptPlay == false) {
     var isPeran = false;
     var isPlay = prompt('Apa kamu pemain baru? [Y]/[N]');
-    if (isPlay == 'y' || isPlay == 'n' || isPlay == 'Y' || isPlay == 'N') {
+    if (isPlay == 'y' || isPlay == 'Y') {
         while (isPeran == false) {
-            var userPeran = prompt('Pilih peran kamu? \t[1] Kstaria \t[2] Tabib \t[3] Penyihir');
+            var userPeran = prompt('Pilih peran kamu? \n[1] Kstaria \n[2] Tabib \n[3] Penyihir');
             if (userPeran == 1) {
-                console.log('kamu memilih peran ' + peran1);
+                userPeran = peran1;
+                console.log('kamu memilih peran ' + userPeran);
                 isPeran = true;
             } else if (userPeran == 2) {
-                console.log('kamu memilih peran ' + peran2);
+                userPeran = peran2;
+                console.log('kamu memilih peran ' + userPeran);
                 isPeran = true;
             } else if (userPeran == 3) {
-                console.log('kamu memilih peran ' + peran3);
+                userPeran = peran3;
+                console.log('kamu memilih peran ' + userPeran);
                 isPeran = true;
             } else {
                 alert('kamu belum memilih peran');
                 isPeran = false;
             }
-       
+        }
+        if (userPeran == peran1) {
+            var senjata = 'kamu dapat menyerang dengan senjatamu.';
+        } else if (userPeran = peran2) {
+            var senjata = 'kamu akan membantu temanmu yang terluka.';
+        } else if (userPeran = peran3) {
+            var senjata = 'ciptakan keajaiban keajaiban yang membantu kemenanganmu!';
+        } else {
+            console.log('tidak ada peran');
         }
         console.log('peran terpilih');
-        var namaUser = prompt('Siapa nama kamu');
-        console.log('nama user: '+ namaUser);
+        var isNama = false;
+        while (isNama == false) {
+            var userNama = prompt("Namai dirimu");
+            if (userNama != null) {
+                isNama = true;
+            } else {
+                alert("Isikan nama kamu");
+                isNama = false;
+            }
+        }
+        console.log('nama user: ' + userNama);
+        console.log('peran: ' + userPeran);
         alert('pemilihan peran selesai');
         alert(
-            '**SUMMARY** \n========= \nNama: '+ namaUser+'\nperan:  '+userPeran
+            '**SUMMARY** \n============ \nHalo '+userNama+', Selamat datang di Dunia Proxytia, Kamu berperan sebagai ' + userPeran + ', ' + senjata
         );
-        promptPlay = false;
+        promptPlay = true;
+
     } else {
         console.log('kamu memilih untuk tidak bermain');
+        alert("Kamu tidak jadi memulai baru");
         promptPlay = true;
-    
+
     }
-    
-    
+
+
 }
+
 console.log("------------------");
 console.log("PROGRAM SELESAI");
